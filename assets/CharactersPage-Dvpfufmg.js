133a151b6ef5813bf314b0a25333c0b385136068
Necmettin <necmettin.dev@gmail.com>
2025-07-28 11:04:35 +0300

    Updates

---
+++ assets/CharactersPage-Dvpfufmg.js
@@ -0,0 +1,1 @@
+import{B as a}from"./BaseLayout-D3rEtYAi.js";import{a as e,g as o,o as r}from"./index-rbNv5i8x.js";const _=e({__name:"CharactersPage",setup(t){return(s,c)=>(r(),o(a))}});export{_ as default};
