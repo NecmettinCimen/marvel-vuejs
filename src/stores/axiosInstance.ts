059a0bb03ca496b142bdc140c827e4e88ade0693
Necmettin Çimen <necmettin.dev@gmail.com>
2026-04-05 20:46:54 +0300

    fix: update axiosInstance.ts

--- src/stores/axiosInstance.ts
+++ src/stores/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://nec-ocelot-gateway.necmettincimen.xyz',
+  baseURL: 'https://nec-ocelot-gateway.necmettincimen.com.tr',
 })
 export default instance
